7629a8f91487507b1d8a93b50a3e7d144841e8e8
João Barbosa <joaobarbosa>
2016-11-18 16:01:07 -0200

    Initial setup for packaging

---
+++ onesignal_client/version.py
@@ -0,0 +1,2 @@
+version_info = (0, 0, 1, 'dev1')
+__version__ = '.'.join(str(v) for v in version_info)
---
+++ setup.py
@@ -0,0 +1,55 @@
+"""OneSignal Python setup module.
+
+See:
+https://github.com/joaobarbosa/onesignal-python
+"""
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+__version__ = None
+with open('onesignal_client/version.py') as f:
+    exec(f.read())
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    # Project
+    name='onesignal-python',
+    version=str(__version__),
+    description='Python client for OneSignal push notification service',
+    long_description=long_description,
+    url='https://github.com/joaobarbosa/onesignal-python',
+
+    # Author
+    author='João Barbosa',
+    author_email='joao.ofb@gmail.com',
+    license='MIT',
+
+    # Classifiers - https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        'Development Status :: 2 - Pre-Alpha',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        # License
+        'License :: OSI Approved :: MIT License',
+        # Python versions you support
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='onesignal client push notifications api',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Requirements - https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],
+
+    # List additional groups of dependencies here - pip install -e .[dev,test]
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
+)
