4b6adcf89d2e893bbe31a8d9f1e5f8aa77d2b39d
João Barbosa <joaobarbosa>
2016-11-30 15:02:00 -0200

    Better doc

--- onesignalclient/base_client.py
+++ onesignalclient/base_client.py
@@ -8,12 +8,18 @@ class OneSignalBaseClient():
     MODE_USER = 'user'
 
     def _url(self, endpoint):
+        """
+        Build the full OneSignal API URL.
+        :return: Returns the complete url string
+        :rtype: str
+        """
         return 'https://onesignal.com/api/v1/%s' % endpoint
 
     def _get_headers(self):
         """
         Build default headers for requests. Fallback to "app" mode
         :return: Returns dict which contains the headers
+        :rtype: dict
         """
         auth = "Basic %s" % self.auth_key if self.mode == self.MODE_USER \
             else self.app_api_key
@@ -25,6 +31,12 @@ class OneSignalBaseClient():
         return headers
 
     def get(self, url):
+        """
+        Perform a GET request.
+        :return: Returns json response
+        :rtype: dict or list
+        :raises requests.exceptions.HTTPError: if status code is not 2xx
+        """
         request = requests.get(url)
         request.raise_for_status()
         return request.json()
